c16b34b8cddda35b47a92fc13a8b31d9064b55bd
Sofiane SADOUD (Kabylixx) <sofiane.sadoud@gmail.com>
2022-08-25 16:35:43 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:tests/MarsRoverTest.php

---
+++ tests/MarsRoverTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace MarsRover;
+
+use PHPUnit\Framework\TestCase;
+
+class MarsRoverTest extends TestCase
+{
+
+    public function testItExecutesSomething(): void
+    {
+        // Arrange
+        $marsRover = new MarsRover();
+        $command = '';
+
+        // Act
+        $commandResult = $marsRover->execute($command);
+
+        // Assert
+        $this->assertEquals('0:0:N', $commandResult);
+    }
+}
